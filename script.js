b98068c908101eda991b5901a7178cb999902faa
LakshanPramodya <lakshanpramodya93@gmail.com>
2022-07-13 15:43:33 +0530

    Added Welcome

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+const mobileBtn = document.getElementById("mobile-cta");
+nav = document.querySelector("nav");
+mobileBtnExit = document.getElementById("mobile-exit");
 
-    const mobileBtn = document.getElementById('mobile-cta')
-    nav = document.querySelector('nav')
-    mobileBtnExit = document.getElementById('mobile-exit');
-    
-    mobileBtn.addEventListener('click', () => {
-        nav.classList.add('menu-btn');
-    })
-    
-    mobileBtnExit.addEventListener('click', () => {
-        nav.classList.remove('menu-btn');
-    })
+mobileBtn.addEventListener("click", () => {
+  nav.classList.add("menu-btn");
+});
+
+mobileBtnExit.addEventListener("click", () => {
+  nav.classList.remove("menu-btn");
+});
+
+console.log("Welcome");
